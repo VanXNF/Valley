a1011913fb65a0556564a5f1a17b1997d24579a4
VanXNF <VanXNF@gmail.com>
2018-03-10 22:36:15 +0800

    确认布局

--- app/src/main/java/com/vanxnf/photovalley/MainActivity.java
+++ app/src/main/java/com/vanxnf/photovalley/MainActivity.java
@@ -1,13 +1,131 @@
 package com.vanxnf.photovalley;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.vanxnf.photovalley.widget.Tablayout.SlideTabLayout;
+import com.vanxnf.photovalley.widget.Tablayout.LineMoveIndicator;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    private DrawerLayout mDrawerLayout;
+
+    private static final String DOG_BREEDS[] = {"Pug", "Beagle", "Bulldog", "Poodle"};
+    private ViewPager viewPager;
+    private SlideTabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setOverflowIcon(getDrawable(R.drawable.ic_layout_manager_dark));
+        setSupportActionBar(mToolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_dark);
+        }
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+
+        tabLayout = (SlideTabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    /** 显示 Toolbar Menu 图标*/
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (menu != null) {
+            if (menu.getClass().getSimpleName().equalsIgnoreCase("MenuBuilder")) {
+                try {
+                    Method method = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    method.setAccessible(true);
+                    method.invoke(menu, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
+    /** 创建 Toolbar Menu*/
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    /** Toolbar 右上角 Menu Item 点击事件*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                break;
+            case R.id.flow_layout:
+                break;
+            case R.id.sg_layout:
+                break;
+            default:
+        }
+        return true;
+    }
+
+    public void onClickLineMove(View view){
+        tabLayout.setAnimatedIndicator(new LineMoveIndicator(tabLayout));
+    }
+
+
+    public class PagerAdapter extends FragmentStatePagerAdapter {
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return new PageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return DOG_BREEDS.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return DOG_BREEDS[position];
+        }
+    }
+
+    public static class PageFragment extends Fragment {
+
+        public PageFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_page, container, false);
+        }
+    }
+
 }
